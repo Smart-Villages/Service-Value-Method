c869fac1d01d85b5d3f687ffef7a662a081f893b
Thiemo Müller <thiemo@edge-box.com>
2020-01-15 16:31:15 +0100

    Made footer and instructions configurable

--- src/Pages/Start.js
+++ src/Pages/Start.js
@@ -32,79 +32,57 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  */
 class Start extends React.Component {
   /**
+   * Initialize the state. Load the external HTML files.
+   *
+   * @param props
+   */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      instructions: "",
+      footer: "",
+    };
+
+    const loadHtml = (name) => {
+      fetch(`/${name}.html`)
+        .then(res => res.text())
+        .then(
+          (result) => {
+            this.setState({
+              [name]: result,
+            });
+          },
+          (error) => {
+            this.setState({
+              [name]: `<em class='error'>Failed to load ${name}.</em>`,
+            });
+          }
+        );
+    };
+
+    loadHtml("instructions");
+    loadHtml("footer");
+  }
+
+  /**
    * Default render() callback.
    *
    * @returns {*}
    */
   render() {
+    /**
+     * We're using `dangerouslySetInnerHTML` where the HTML is loaded from a file that's on the same web server, so it
+     * doesn't introduce any additional security risks.
+     */
     return (
       <div className="App-Start">
         <h1>Step 1/3: Upload CSV file</h1>
-        <h2>How to</h2>
-        <ul>
-          <li>Upload a *.csv file. To get the csv file, export it from Excel by using "Save as...". The csv file should have
-            the following structure:</li>
-          <li>Provide a header row with the names of the services that the participants of your focus group came up with/could
-            choose from. We recommend using short labels for the names of the services to make the service map as readable as possible.
-          </li>
-          <li>Provide one row for each participant. The name of the participant (can be anonymised but must be unique) must be
-            in the first column, then insert the number of counters they assigned to each service. If participants could only
-            select Yes or No, put in a 1 (Yes) or 0 (No).
-          </li>
-          <li>In rows at the bottom, provide your Decision Criteria that you want to assess each service with respect to. Examples
-            are "Ease of delivery" or "Near-term economic benefit" (as defined in Clements et al 2019). For each service, insert a
-            number to rate the service. When ranking services according to a Decision Criterion, larger numbers are interpreted as better.
-          </li>
-          <li>Optional: To control the colour of the service bubbles, add an additional row titled 'Colour' and indicate with numbers
-            which services should be the same colour. For example, if you held multiple focus groups you can use the colour of each
-            service bubble to indicate how many of the groups suggested that service.
-          </li>
-        </ul>
-        <h2>Example:</h2>
-        <table>
-          <thead>
-          <tr>
-            <th>Person</th>
-            <th>Option 1</th>
-            <th>Option 2</th>
-            <th>Option 3</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <th>Participant A</th>
-            <td>3</td>
-            <td>0</td>
-            <td>1</td>
-          </tr>
-          <tr>
-            <th>Participant B</th>
-            <td>2</td>
-            <td>1</td>
-            <td>1</td>
-          </tr>
-          <tr>
-            <th>Participant C</th>
-            <td>0</td>
-            <td>1</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <th>Ease of delivery</th>
-            <td>1</td>
-            <td>1</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <th>Near-term economic benefit</th>
-            <td>2</td>
-            <td>1</td>
-            <td>1</td>
-          </tr>
-          </tbody>
-        </table>
-        <h2>Get started:</h2>
+        <div dangerouslySetInnerHTML={{
+          __html: this.state.instructions
+        }} />
 
+        <h2>Get started:</h2>
         <div className="App-Upload">
           <div className="Background">Upload...</div>
           <CSVReader onFileLoaded={data => {
@@ -179,13 +157,9 @@ class Start extends React.Component {
           }}/>
         </div>
 
-        <p className="Copyright">
-          Clements, A., Wheeler, S., Mohr, A., & McCulloch, M. (2019). The Service Value Method for Design of Energy Access Systems in the Global South. Proceedings of the IEEE, 107(9), 1941–1966. <a href="https://doi.org/10.1109/JPROC.2019.2901215">https://doi.org/10.1109/JPROC.2019.2901215</a>
-        </p>
-
-        <p className="Copyright">
-          Software copyright &copy; 2020 Thiemo Müller. This is free software, licensed under the <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">AGPL-3.0-or-later</a>. You can view and download the source code <a href="https://github.com/Smart-Villages/Service-Value-Method">here</a>.
-        </p>
+        <div dangerouslySetInnerHTML={{
+          __html: this.state.footer
+        }} />
       </div>
     );
   }
